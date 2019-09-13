6390a12830727d1abaf86fac298db7e3a6e29ca9
Alexis Panyathong <alexispanyathong@gmail.com>
2019-09-13 10:44:35 -0500

    New branch
    I just copied and pasted what the new changes were into this vs code and now I am ready to start.

--- smurfs/src/components/App.js
+++ smurfs/src/components/App.js
@@ -1,48 +1,20 @@
-import React, { useReducer, useEffect } from "react";
-import "./App.css";
-import axios from "axios";
-
-import { initialState, reducer } from '../components/reducers/SmurfReducer';
-import NewSmurfForm from "./NewSmurfForm";
-import Smurf from './Smurf';
-
-import { Title } from './StyledWidgets';
-
-
-function App () {
-  
-  const [state, dispatch] = useReducer(reducer, initialState);
-
-
-
-  useEffect(() => {
-    //Adding axios GET request HERE
-    axios.get('http://localhost:3333/smurfs')
-      .then(res => {
-        console.log('res api', res.data);
-        dispatch({ type: 'ADD_SMURF', payload: res.data});
-
-
-      })
-      .catch(err => console.log('Error, please try again', err))
-
-  }, [])
-
-  
-    return (
-      <div className="App">
-            <Title>
-           <h1>Welcome to the Smurf Registration Form:</h1>
-          </Title>
-
-          <NewSmurfForm />
-          
-          {state.smurf.map(smurf => (
-            <Smurf key={smurf.id} smurf={smurf} />
-          ))}
+import React, { Component } from 'react';	import React, { Component } from "react";
+import './App.css';	import "./App.css";
+/*	
+ to wire this component up you're going to need a few things.	
+ I'll let you do this part on your own. 	
+ Just remember, `how do I `connect` my components to redux?`	
+ `How do I ensure that my component links the state to props?`	
+ */	
+class App extends Component {	
+  render() {	  
+    return (	    
+        <div className="App">
+          <h1>SMURFS! 2.0 W/ Redux</h1>	        
+          <div>Welcome to your Redux version of Smurfs!</div>
+          <div>Start inside of your `src/index.js` file!</div>
+          <div>Have fun!</div>	        
       </div>
-    );
+    )
+  } 
 }
-
-
-export default App;
